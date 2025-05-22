a6f723dab5e71961e07c255492a88bf07a657f02
iruziky <iruziky@gmail.com>
2025-05-22 00:05:42 -0300

    chore: update requirements and config

--- src/config.py
+++ src/config.py
@@ -1,3 +1,9 @@
-BASE_URL = "https://rn.olx.com.br"
 HEADLESS = False
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
+
+# Get from back-end
+STATE = "rio-grande-do-norte"
+STATE_ABBREVIATION = "rn"
+CITY = "natal"
+
+BASE_URL = f"https://www.olx.com.br/celulares/estado-{STATE_ABBREVIATION}/{STATE}/{CITY}/q=smartphone&sf=1"
