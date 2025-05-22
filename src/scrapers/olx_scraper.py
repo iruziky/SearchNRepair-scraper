b4836cdc4dbeb022d79790e1369439ad0d972332
iruziky <iruziky@gmail.com>
2025-05-22 18:44:10 -0300

    remove bug

--- src/scrapers/olx_scraper.py
+++ src/scrapers/olx_scraper.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from config import BASE_URL, USER_AGENT, HEADLESS
+from config import BASE_URL
 import time
 
 def get_page(driver):
