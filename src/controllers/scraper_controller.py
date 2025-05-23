978df13a0efe1f1ee6bcdf860c4934e04bf88461
iruziky <iruziky@gmail.com>
2025-05-23 18:57:19 -0300

    feat(scraper): add error handling and longer delay between requests

--- src/controllers/scraper_controller.py
+++ src/controllers/scraper_controller.py
@@ -1,20 +1,8 @@
 from scrapers.olx_scraper import get_page, next_page, is_last_page
 from storage.csv_writer import save_to_csv
 from parsers.olx_parser import extract_links 
-from parsers.product_parser import (
-    get_title,
-    get_price,
-    get_category,
-    get_brand,
-    get_model,
-    get_condition,
-    get_memory,
-    get_color,
-    get_battery_life,
-    get_description,
-    get_url_images,
-)
-
+from selenium.common.exceptions import WebDriverException
+from parsers.product_parser import *
 import time
 import random
 import requests
@@ -33,6 +21,7 @@ def search_page_scraper(driver):
 
             print(f"Page {index} with {len(links)} links")
 
+            # Não tá identificando a última página
             if is_last_page(driver):
                 print("Last page reached.")
                 break
@@ -54,32 +43,43 @@ def products_scraper(driver):
     if not links:
         print("No links found for scraping.")
         return
-    
-    products_data = []
+
     for i, link in enumerate(links, 1):
         print(f"\nScraping product {i}/{len(links)}: {link}")
-        driver.get(link)
         
-        product_data = {
-            "link": link,
-            "title": get_title(driver),
-            "price": get_price(driver),
-            "category": get_category(driver),
-            "brand": get_brand(driver),
-            "model": get_model(driver),
-            "condition": get_condition(driver),
-            "memory": get_memory(driver),
-            "color": get_color(driver),
-            "batteryLife": get_battery_life(driver),
-            "description": get_description(driver),
-            "images": get_url_images(driver)
-        }
+        try:
+            driver.get(link)
+        except WebDriverException as e:
+            print(f"Failed to load page {link}: {e}")
+            delay = random.uniform(25, 35)
+            print(f"Aguardando {delay:.2f} segundos antes de continuar...")
+            time.sleep(delay)
+            continue
 
-        url = "https://olx-back-end.onrender.com/smartphones/save"
+        try:
+            product_data = {
+                "link": link,
+                "title": get_title(driver),
+                "price": get_price(driver),
+                "category": get_category(driver),
+                "brand": get_brand(driver),
+                "model": get_model(driver),
+                "condition": get_condition(driver),
+                "memory": get_memory(driver),
+                "color": get_color(driver),
+                "batteryLife": get_battery_life(driver),
+                "description": get_description(driver),
+                "images": get_url_images(driver)
+            }
 
-        response = requests.post(url, json=product_data)
+            url = "https://olx-back-end.onrender.com/smartphones/save"
+            response = requests.post(url, json=product_data, timeout=10)
+            print("Status code:", response.status_code)
+            print("Response body:", response.text)
 
-        print("Status code:", response.status_code)
-        print("Response body:", response.text)
+        except Exception as e:
+            print("Erro ao coletar dados ou enviar para API:", e)
 
-        time.sleep(random.uniform(3, 6))
+        delay = random.uniform(25, 35)
+        print(f"Aguardando {delay:.2f} segundos antes de continuar...")
+        time.sleep(delay)
